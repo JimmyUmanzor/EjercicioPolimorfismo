3b10aba279628778e7042932df11576c8eb7da3a
JimmyUmanzor <159740825+JimmyUmanzor@users.noreply.github.com>
2024-05-19 22:49:04 -0600

    Versión Final

--- Animal.cs
+++ Animal.cs
@@ -17,12 +17,12 @@ namespace EjercicioPolimorfismo
 
         public virtual void HacerSonido()
         {
-            Console.WriteLine("El animal hace un sonido.");
+            Console.WriteLine("El animal hace un sonido");
         }
 
         public virtual void Moverse()
         {
-            Console.WriteLine("El animal se mueve.");
+            Console.WriteLine("El animal se mueve");
         }
     }
 }
